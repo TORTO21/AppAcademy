9a391618d5ad1daf5abe512c8c24ff3116dd68a0
Oliver Almalel <oliver.almalel@gmail.com>
2019-03-19 22:19:57 -0700

    homework and updates

--- W5D1/ghost_game_solution/player.rb
+++ W5D1/ghost_game_solution/player.rb
@@ -1,18 +1,33 @@
-
 class Player
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @score = 0
   end
 
-  def guess
+  def alert_invalid_move(letter)
+    puts "#{letter} is not a valid move!"
+    puts "Your guess must be a letter of the alphabet."
+    puts "You must be able to form a word starting with the new fragment.\n"
+  end
+
+  def guess(fragment)
+    prompt(fragment)
     gets.chomp.downcase
   end
 
-  def alert_invalid_guess
-    puts "Invalid guess!"
-    guess
+  def inspect
+    "HumanPlayer: #{name}"
   end
 
+  def to_s
+    name
+  end
+
+  private
+
+  def prompt(fragment)
+    puts "The current fragment is '#{fragment}'."
+    print "Add a letter: "
+  end
 end
