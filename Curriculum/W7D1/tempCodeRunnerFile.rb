c7860e4f11752324bdb2c92a3cf90823c4ad746a
Oliver Almalel <oliver.almalel@gmail.com>
2019-03-26 08:48:49 -0700

    updated file structure

--- Curriculum/W7D1/tempCodeRunnerFile.rb
+++
@@ -1,9 +0,0 @@
-def my_min(list)
-  min = list.first
-  (1...list.length - 1).each do |first|
-    (first...list.length).each do |second|
-      first < second ? min = first : min = second
-    end
-  end
-  min
-end
