acd72f1bda0099007a3c55ef86cb3b3687443b67
Oliver Almalel <oliver.almalel@gmail.com>
2019-04-19 23:27:21 -0700

    end of day late

--- Curriculum/03 Rails/W10D5/rails_lite/lib/controller_base.rb
+++ Curriculum/03 Rails/W10D5/rails_lite/lib/controller_base.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext'
 require 'active_support/inflector'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
@@ -25,6 +26,7 @@ class ControllerBase
       @res.status = 302
       @res.add_header('location', url)
       @already_built_response = true
+      @session.store_session(@res)
     else
       raise RuntimeError
     end
@@ -39,6 +41,7 @@ class ControllerBase
       @res.add_header('content-type', content_type)
       @res.write content
       @already_built_response = true
+      @session.store_session(@res)
     else
       raise RuntimeError
     end
@@ -47,11 +50,17 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
-    template = underscore(template_name)
+    template = template_name.to_s
+    controller = ActiveSupport::Inflector.underscore(self.class.to_s)
+    root = File.dirname(__FILE__)
+    view = File.join(root, "..", "views", controller, "#{template}.html.erb")
+    content = ERB.new(File.read(view)).result(binding)
+    render_content(content, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
