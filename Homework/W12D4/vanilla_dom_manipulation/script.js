204824b7e4a7178763690a70c6143961f28ba0eb
Oliver Almalel <oliver.almalel@gmail.com>
2019-05-02 17:11:40 -0700

    W12D4 eod

--- Homework/W12D4/vanilla_dom_manipulation/script.js
+++ Homework/W12D4/vanilla_dom_manipulation/script.js
@@ -12,11 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll("#restaurants li").forEach((li) => {
     li.addEventListener("click", toggleLi);
+    
+    
+  const ul = document.getElementById("restaurants")
+  const newLi = document.createElement("li")
+
   });
-
-
-
-  // adding SF places as list items
+    // adding SF places as list items
 
   // --- your code here!
 
