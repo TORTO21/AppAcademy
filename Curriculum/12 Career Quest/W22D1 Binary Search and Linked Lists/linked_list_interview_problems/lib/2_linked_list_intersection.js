1b03483fd75acee860e9b2f805903ec0b2ec22b3
Oliver Almalel <oliver.almalel@gmail.com>
2019-07-18 08:09:48 -0700

    W21D5 morning update

--- Curriculum/12 Career Quest/W22D1 Binary Search and Linked Lists/linked_list_interview_problems/lib/2_linked_list_intersection.js
+++ Curriculum/12 Career Quest/W22D1 Binary Search and Linked Lists/linked_list_interview_problems/lib/2_linked_list_intersection.js
@@ -42,21 +42,21 @@
 function linkedListIntersection(list1, list2) {
   // TODO: Implement the hasCycle function!
   const list1Length = list1.length
-  const list2Length = list1.length
+  const list2Length = list2.length
   let list1Node = list1.head
   
   for (let i = 0; i < list1Length; i++) {
     let list2Node = list2.head
     // console.log(i)
     for (let j = 0; j < list2Length; j++) {
-      console.log(j)
+      // console.log(j)
       if (list1Node == list2Node) return list1Node
       list2Node = list2Node.next
     }
     list1Node = list1Node.next
   }
   return null
-}
+}``
 
 // ----------------------------------------
 // Given: Singly Linked List - Do Not Edit!
